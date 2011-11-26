c9a5822c85ccd3de2381ef7c5af8d87e2ed1eb47
Lucas Martin <lucas.martin@cogshift.com>
2011-11-26 21:40:55 +1000

    Reorganised script files.

--- Javascript/src/cog.js
+++ Javascript/src/cog.js
@@ -1,7 +1,7 @@
 ﻿/// <reference path="jquery-1.5.1-vsdoc.js" />
 
 /**
-* Contains a number of useful utiity methods for javascript.
+* The root namespace of the Cognitive Shift javascript library.
 *
 * File:         cog.utils.js
 * Version:      0.1
@@ -12,39 +12,4 @@
 */
 
 
-
-var cog = new function () {
-    this.string = new function () {
-        this.format = function () {
-            var s = arguments[0];
-            for (var i = 0; i < arguments.length - 1; i++) {
-                var reg = new RegExp("\\{" + i + "\\}", "gm");
-                s = s.replace(reg, arguments[i + 1]);
-            }
-
-            return s;
-        };
-
-        this.endsWith = function (string, suffix) {
-            return (string.substr(string.length - suffix.length) === suffix);
-        };
-
-        this.startsWith = function (string, prefix) {
-            return (string.substr(0, prefix.length) === prefix);
-        };
-
-        this.trimEnd = function (string, chars) {
-            if (this.endsWith(string, chars))
-                return string.substring(0, string.length - chars.length);
-
-            return string;
-        };
-
-        this.trimStart = function (string, chars) {
-            if (this.startsWith(string, chars))
-                return string.substring(chars.length, string.length);
-
-            return string;
-        };
-    };
-};
+var cog = {};
